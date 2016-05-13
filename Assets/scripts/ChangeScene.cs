e5625f9d77df46cb2ec89e61471e258af48f141c
nielsvdmarel <nielsvdmarel@gmail.com>
2016-05-13 14:11:17 +0200

    Background, moederboord (met animatie!!) en player art erin. ook wat fixes

---
+++ Assets/scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public void NielsTestScene()
+    {
+        SceneManager.LoadScene("NielsTestScene");
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("main menu");
+    }
+
+}
