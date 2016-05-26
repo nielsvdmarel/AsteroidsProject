7fae5ca610c6b76a52081915d30aa26d1d53db12
nielsvdmarel <nielsvdmarel@gmail.com>
2016-05-26 12:02:48 +0200

    minimap kleine aanpassing testen

---
+++ Assets/MinimapFollowNoRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapFollowNoRotate : MonoBehaviour {
+
+    public GameObject game2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    
+     {
+        this.transform.position = game2.transform.position;
+        
+    }
+}
