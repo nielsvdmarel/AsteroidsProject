3e6d5e6aca3551ccafae8c6a5a75f9480961beb4
CoenSteuns <c.steuns@gmail.com>
2016-06-07 17:03:26 +0200

    niets af, tussen commit

---
+++ Assets/laserShooting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laserShooting : MonoBehaviour {
+
+    public GameObject headLaser;
+
+    private int laserCounter;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Instantiate(headLaser);
+            laserCounter++;
+        }
+
+    }
+
+    public int laserlengtget()
+    {
+        return laserCounter;
+    }
+}
---
+++ Assets/laserscript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laserscript : MonoBehaviour {
+
+    public GameObject laser;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+        GameObject player = GameObject.Find("Player");
+        this.transform.parent = player.transform;
+        this.transform.position = new Vector2(0,0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
