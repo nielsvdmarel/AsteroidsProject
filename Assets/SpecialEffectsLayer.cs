b8e0e71bda7a3f9d0912699b5870b2798f3cf26b
nielsvdmarel <nielsvdmarel@gmail.com>
2016-06-10 15:01:38 +0200

    nodige commit, paar licht effecten test

---
+++ Assets/SpecialEffectsLayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialEffectsLayer : MonoBehaviour
+{
+    void Start()
+    {
+        //Change Foreground to the layer you want it to display on 
+        //You could prob. make a public variable for this
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "halo";
+    }
+
+}
+
