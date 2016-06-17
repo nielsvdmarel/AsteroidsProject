7c1edb2b673f263314e3f0a97af3c948d10dec5c
CoenSteuns <c.steuns@gmail.com>
2016-06-17 15:09:21 +0200

    sound begin

---
+++ Assets/backgroudSound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroudSound : MonoBehaviour {
+
+    public AudioClip[] bulletSounds;
+    private AudioSource audioSourceComponent;
+    public bool LastSound;
+
+    // Use this for initialization
+    void Start () {
+        LastSound = false;
+        audioSourceComponent = this.GetComponent<AudioSource>();
+        audioSourceComponent.PlayOneShot(bulletSounds[0], 0.7F);
+        InvokeRepeating("mid", bulletSounds[0].length-0.1f, bulletSounds[1].length-0.1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+
+	}
+
+    private void mid()
+    {
+        audioSourceComponent.PlayOneShot(bulletSounds[1], 0.7F);
+    }
+}
--- Assets/scripts/bulletsShooting.cs
+++ Assets/scripts/bulletsShooting.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class bulletsShooting : MonoBehaviour {
 
     public GameObject BulletPrefab;
+    public AudioClip[] bulletSounds;
+    private AudioSource audioSourceComponent;
     //private Vector3 pos;
     //private Quaternion rot;
 
     void Start()
     {
-        //rot = transform.rotation;
+        audioSourceComponent = this.GetComponent<AudioSource>();
     }
 
     void Update()
@@ -21,6 +23,7 @@ public class bulletsShooting : MonoBehaviour {
             if (GameObject.FindGameObjectsWithTag("laser").Length < 1)
             {
                 Instantiate(BulletPrefab, transform.position, transform.rotation);
+                audioSourceComponent.PlayOneShot(bulletSounds[Random.Range(0,3)], 0.7F);
             }
         }
     }
