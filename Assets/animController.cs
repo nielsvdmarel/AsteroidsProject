e68a98f22da0ee575c8c637b65a8a36aef72ac6c
nielsvdmarel <nielsvdmarel@gmail.com>
2016-06-17 10:20:57 +0200

    prefabs animaties kleine aanpassingen

---
+++ Assets/animController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animController : MonoBehaviour
+{
+
+    private Animator anim;
+
+
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+
+
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("testbutton");
+            anim.SetBool("die", true);
+
+        }
+        else
+        {
+            anim.SetBool("die", false);
+        }
+    }
+}
+
